cfdf6ca143d2730c1f45dcf1f0664b29965d1f23
Tom Holmes <tholmes59@gmail.com>
2019-01-23 22:24:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    sentence = self.split(/['.']/) || self.split(/['?']/) || self.split(/['!']/)
+    sentence = self.split(/['.']/) && self.split(/['?']/) && self.split(/['!']/)
     number_of_sentences = sentence.reject {|sentence| sentence.empty?}
     number_of_sentences.count
   end
