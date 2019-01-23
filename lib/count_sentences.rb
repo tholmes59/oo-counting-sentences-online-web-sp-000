b591d6509358c454978ec58f3c743042c5481d55
Tom Holmes <tholmes59@gmail.com>
2019-01-23 22:20:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true 
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentence = self.split('.') || self.split('?') || self.split('!')
+    number_of_sentences = sentence.reject {|sentence| sentence.empty?}
+    number_of_sentences.count
   end
+
 end
